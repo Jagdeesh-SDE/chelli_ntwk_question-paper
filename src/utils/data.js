b3c9b8b2851674c701c2a152b74676e4918c0c13
Jagadeesh-SDE <jagadeesh.ashokit@gmail.com>
2024-06-22 00:58:44 +0530

    added almost all the questions

--- src/utils/data.js
+++ src/utils/data.js
@@ -256,8 +256,210 @@ export const questions = [
   },
   {
     id: 27,
-    question: "JSX stands for __________",
-    options: ["Javascript XML", "JSON XML", "JSON", "Javascript and AngularJS"],
+    question:
+      "From the given options, identify core capabilities of cloud (Select TWO CORRECT options) ",
+    options: [
+      "Rapid Elasticity",
+      "Reliability",
+      "Security",
+      "Resource poolingS",
+    ],
     answer: "Javascript XML",
+  },
+  {
+    id: 28,
+    question: "Name few popular PAAS offerings (Select TWO CORRECT options)",
+    options: ["LinkedIn", "Pixlr", "IBM Bluemix", "Google App Engine"],
+    answer: "Javascript XML",
+  },
+  {
+    id: 29,
+    question: "Which layer belongs to SAAS cloud service model?",
+    options: [
+      "Application and Data",
+      "Application and Runtime Environment",
+      "Storage and Data",
+      "Networking and Operating systems",
+    ],
+    answer: "Application and Data",
+  },
+  {
+    id: 30,
+    question:
+      "Which of the following options does not represent cloud service models?",
+    options: ["IAAS", "PAAS", "ISP", "SAAS"],
+    answer: "ISP",
+  },
+  {
+    id: 31,
+    question:
+      "A network is configured with VLANs 10, 20, 30, 40 & 50 with at least 1 EUD respectively. Which among the following commands is to be executed to allow traffic of VLANID 10,30 and 50 only in a pc",
+    options: [
+      "switchport access vlan 10 30 50",
+      "switchport mode trunk valn 1-10,30,50",
+      "switchport mode trunk allowed VLAN 1-10,30,50",
+      "switchport mode access",
+    ],
+    answer: "switchport mode trunk allowed VLAN 1-10,30,50",
+  },
+  {
+    id: 32,
+    question:
+      "In a network, 2 host devices are configured to VLAN 1O and VLAN 20 respectively. Which among the following statements is wrong in this scenario ?",
+    options: [
+      "Hosts can communicate between them only if they are configured under the same VLAN ID",
+      "Hosts can communicate between them only if they are configured under Default VLAN ID only",
+      "Hosts can communicate between them without any changes",
+      "Hosts cant communicate between them in this scenario",
+    ],
+    answer: "Hosts can communicate between them without any changes",
+  },
+  {
+    id: 33,
+
+    question: "Name few popular PAAS offerings (Select TWO CORRECT options)",
+    options: ["LinkedIn", "Pixlr", "IBM Bluemix", "Google App Engine"],
+    answer: "IBM Bluemix",
+    // answer: ["IBM Bluemix", "Google App Engine"],
+  },
+  {
+    id: 34,
+    question:
+      "How does a Cisco Unified Wireless Network respond to Wi-Fi channel ovelap?",
+    options: [
+      "It allows the administrator to assign the channels on a per-device or per-interface basis.",
+      "It segregates devices from different manufactures onto different channels.",
+      "It analyzes client load and background noise and dynamically assigns a channel.",
+      "It alternates automatically between 2.4 GHz and 5 GHz on adjacent access points.",
+    ],
+    answer:
+      "It analyzes client load and background noise and dynamically assigns a channel.",
+  },
+  {
+    id: 35,
+
+    question:
+      "Which function forwards frames to ports that have a matching destination MAC address?",
+    options: [
+      "frame flooding",
+      "frame filtering",
+      "frame pushing",
+      "frame switching",
+    ],
+    answer: "frame switching",
+  },
+  {
+    id: 36,
+    question: "which of the following VLAN IDs fall into the Normal Range?",
+    options: ["1001", "1003", "1005", "1007"],
+    answer: "frame switching",
+  },
+  {
+    id: 37,
+    question:
+      "Which mode should be configured for the ports that are connected to an EUD and a Switch, respectively?",
+    options: [
+      "Access Mode & Trunk Mode",
+      "Trunk Mode & Access Mode",
+      "Access Mode & Access Mode",
+      "Trunk Mode & Trunk Mode",
+    ],
+    answer: "frame switching",
+  },
+  {
+    id: 38,
+    question: "In which CLI mode. trunk mode can be configured for a port?",
+    options: [
+      "User Mode",
+      "Privilege Mode",
+      "Global configuration mode",
+      "specific configuration mode",
+    ],
+    answer: "frame switching",
+  },
+  {
+    id: 39,
+    question: "How will a Switch update its MAC-Address Table?",
+    options: [
+      "Learns from the Destination MAC Address of a Frame with respect to the exit interface",
+      "Learns from the Source MAC Address of a Frame with respect to the incoming interface",
+      "Learns from the Destination Port Number of a Frame with respect to the exit interface",
+      "Learns from the Source Port Number of a Frame with respect to the incoming interface",
+    ],
+    answer:
+      "Learns from the Source MAC Address of a Frame with respect to the incoming interface",
+  },
+  {
+    id: 40,
+
+    question:
+      "How does a Cisco Unified Wireless Network respond to Wi-Fi channel overlap?",
+    options: [
+      "It allows the administrator to assign the channels on a per-device or per-interface basis.",
+      "It segregates devices from different manufactures onto different channels.",
+      "It analyzes client load and background noise and dynamically assigns a channel.",
+      "It alternates automatically between 2.4 GHz and 5 GHz on adjacent access points.",
+    ],
+    answer:
+      "Learns from the Source MAC Address of a Frame with respect to the incoming interface",
+  },
+  {
+    id: 41,
+
+    question:
+      "Jimmy, a network admin wants to send multiple VLAN traffic between switches. What modes should he configure the switch ports, to achieve this?",
+    options: ["Access", "Trunk", "Both A and B", "None A and B"],
+    answer: "Trunk",
+  },
+  {
+    id: 42,
+
+    question:
+      "What keyword describes the mechanism of a switch used to get the frame to the correct destination device?",
+    options: [
+      "Advancing",
+      "Flooding to the destination",
+      "Forwarding",
+      "Dropping",
+    ],
+    answer: "Forwarding",
+  },
+  {
+    id: 43,
+
+    question:
+      "A global Enterprise network wants to use VLAN in their projects. Out of the given options which VLAN they can use?",
+    options: ["1003", "1002", "1005", "1008"],
+    answer: "1008",
+  },
+  {
+    id: 44,
+
+    question: "What are two functions of a Layer 2 switch? (Choose two.)",
+    options: [
+      "makes forwarding decisions based on the MAC address of a packet",
+      "selects the best route between networks on a WAN",
+      "moves packets within a VLAN",
+      "moves packets between different VLANs",
+    ],
+    answer: "makes forwarding decisions based on the MAC address of a packet",
+    // answer: [
+    //   "makes forwarding decisions based on the MAC address of a packet",
+    //   "moves packets within a VLAN",
+    // ],
+  },
+  {
+    id: 45,
+
+    question:
+      "What will be the course of action of a Switch, If the MAC Address associated with all the connected interfaces is learned and a new switching request for an unknown destination MAC Address comes in?",
+    options: [
+      "The switch forwards the packet and adds the destination MAC address to its table",
+      "The switch sends a copy of a packet to CPU for destination MAC address learning",
+      "The switch floods packets to all interfaces except the receiving interface",
+      "The switch drops the received frame",
+    ],
+    answer:
+      "The switch sends a copy of a packet to CPU for destination MAC address learning",
   },
 ];
