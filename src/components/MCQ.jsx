e018fd098ff0d9b88bd6990826977fa04a8a1412
Jagadeesh-SDE <jagadeesh.ashokit@gmail.com>
2024-06-22 16:35:45 +0530

    made with love for my sister

--- src/components/MCQ.jsx
+++ src/components/MCQ.jsx
@@ -1,9 +1,20 @@
+import { useEffect } from "react";
 import "./MCQ.css";
 import Options from "./Options";
+import { useLocation } from "react-router-dom";
 
 function MCQ({ questions }) {
+  const location = useLocation();
+
+  useEffect(() => {
+    // myRef.current.scrollIntoView({ behavior: "smooth" });
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [location]);
   return (
-    <>
+    <div>
       {questions.map((eachQuestion, i) => (
         <div className="question mb-10 w-[100%]" key={eachQuestion.id}>
           <h2 className="text-base font-medium mb-3">
@@ -12,7 +23,7 @@ function MCQ({ questions }) {
           <Options eachQuestion={eachQuestion} />
         </div>
       ))}
-    </>
+    </div>
   );
 }
 export default MCQ;
